1d96fc24e5b5920e23a088299eb3fb21e39730f7
Trina Chaudhuri <trina@parentlane.com>
2021-03-09 18:50:45 +0530

    added header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
-
+//components
+import Header from './components/header';
+//style
+import './App.css'
 export default function App(){
     return(
-        <div>
-            App
+        <div className="parentcontainer">
+            <Header />
         </div>
     )
 }
